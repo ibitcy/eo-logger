bba1b0a76837ecd7fe7b56a204c9dc19143c1cdf
Maksim Sharipov <m.v.sharipov@gmail.com>
2020-04-28 15:38:05 +0300

    Client utils for collect metrics were added

---
+++ src/client/utils.ts
@@ -0,0 +1,41 @@
+export function getScreenInformation(): Record<string, string> | undefined {
+  try {
+    return {
+      orientation: (window.screen.orientation || {}).type,
+      size: `${window.screen.width}x${window.screen.height}`,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * @see https://www.ibm.com/developerworks/ru/library/bd-r-javascript-w3c/index.html
+ * @see https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp?hl=ru
+ */
+export function getPerformanceMetrics(): Record<string, number> | undefined {
+  try {
+    const {
+      domComplete,
+      domLoading,
+      domInteractive,
+      navigationStart,
+    } = window.performance.timing;
+
+    const metrics: Record<string, number> = {};
+
+    if (domComplete > 0) {
+      metrics.domComplete = domComplete - domLoading;
+      metrics.domCompleteFull = domComplete - navigationStart;
+    }
+
+    if (domInteractive > 0) {
+      metrics.domInteractive = domInteractive - domLoading;
+      metrics.domInteractiveFull = domInteractive - navigationStart;
+    }
+
+    return metrics;
+  } catch (error) {
+    console.error(error);
+  }
+}
