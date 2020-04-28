0c2f60bd1d28332abcf0529e58cca5d9afffe31f
Maksim Sharipov <m.v.sharipov@gmail.com>
2020-04-28 16:31:11 +0300

    Client logger was implemented

---
+++ src/client/logger.ts
@@ -0,0 +1,45 @@
+import { Context as BaseContext } from '../context';
+import { ECS } from '../ecs';
+import { Logger as BaseLogger } from '../logger';
+import { getPerformanceMetrics, getScreenInformation } from './utils';
+
+export class Logger extends BaseLogger {
+  public readonly context = new Context();
+
+  public constructor() {
+    super();
+
+    this.context.setLog({
+      logger: 'ClientLogger',
+    });
+
+    this.context.setUserAgent({
+      original: navigator.userAgent,
+    });
+  }
+
+  public collectMetrics(): void {
+    const ecsMessage = {
+      ...this.context.aggregate(),
+      metrics: getPerformanceMetrics(),
+      screen: getScreenInformation(),
+    };
+
+    this.transport.send(ecsMessage);
+  }
+}
+
+export class Context extends BaseContext {
+  public aggregate(): ECS.Message {
+    const ecsMessage = super.aggregate();
+
+    ecsMessage.url = {
+      fragment: document.location.hash,
+      original: document.location.href,
+      path: document.location.pathname,
+      query: document.location.search,
+    };
+
+    return ecsMessage;
+  }
+}
